a4149efcebeecbf8e9a4b593353688244cd88011
Renan Machado <therbtm@gmail.com>
2017-04-08 20:41:00 -0300

    Add files via upload

---
+++ demo/Debug/src/main.d
@@ -0,0 +1,58 @@
+src/main.o src/main.d: ../src/main.c \
+ C:\Users\renan\Documents\LPCXpresso_8.2.2_650\workspace\Lib_MCU\inc/lpc17xx_pinsel.h \
+ C:\Users\renan\Documents\LPCXpresso_8.2.2_650\workspace\Lib_CMSISv1p30_LPC17xx\inc/LPC17xx.h \
+ C:\Users\renan\Documents\LPCXpresso_8.2.2_650\workspace\Lib_CMSISv1p30_LPC17xx\inc/core_cm3.h \
+ C:\Users\renan\Documents\LPCXpresso_8.2.2_650\workspace\Lib_CMSISv1p30_LPC17xx\inc/system_LPC17xx.h \
+ C:\Users\renan\Documents\LPCXpresso_8.2.2_650\workspace\Lib_MCU\inc/lpc_types.h \
+ C:\Users\renan\Documents\LPCXpresso_8.2.2_650\workspace\Lib_MCU\inc/lpc17xx_gpio.h \
+ C:\Users\renan\Documents\LPCXpresso_8.2.2_650\workspace\Lib_MCU\inc/lpc17xx_i2c.h \
+ C:\Users\renan\Documents\LPCXpresso_8.2.2_650\workspace\Lib_MCU\inc/lpc17xx_ssp.h \
+ C:\Users\renan\Documents\LPCXpresso_8.2.2_650\workspace\Lib_MCU\inc/lpc17xx_adc.h \
+ C:\Users\renan\Documents\LPCXpresso_8.2.2_650\workspace\Lib_MCU\inc/lpc17xx_timer.h \
+ C:\Users\renan\Documents\LPCXpresso_8.2.2_650\workspace\Lib_EaBaseBoard\inc/joystick.h \
+ C:\Users\renan\Documents\LPCXpresso_8.2.2_650\workspace\Lib_EaBaseBoard\inc/pca9532.h \
+ C:\Users\renan\Documents\LPCXpresso_8.2.2_650\workspace\Lib_EaBaseBoard\inc/acc.h \
+ C:\Users\renan\Documents\LPCXpresso_8.2.2_650\workspace\Lib_EaBaseBoard\inc/rotary.h \
+ C:\Users\renan\Documents\LPCXpresso_8.2.2_650\workspace\Lib_EaBaseBoard\inc/led7seg.h \
+ C:\Users\renan\Documents\LPCXpresso_8.2.2_650\workspace\Lib_EaBaseBoard\inc/oled.h \
+ C:\Users\renan\Documents\LPCXpresso_8.2.2_650\workspace\Lib_EaBaseBoard\inc/rgb.h \
+ C:\Users\renan\Documents\LPCXpresso_8.2.2_650\workspace\Lib_EaBaseBoard\inc/uart2.h \
+ C:\Users\renan\Documents\LPCXpresso_8.2.2_650\workspace\Lib_MCU\inc/lpc17xx_uart.h
+
+C:\Users\renan\Documents\LPCXpresso_8.2.2_650\workspace\Lib_MCU\inc/lpc17xx_pinsel.h:
+
+C:\Users\renan\Documents\LPCXpresso_8.2.2_650\workspace\Lib_CMSISv1p30_LPC17xx\inc/LPC17xx.h:
+
+C:\Users\renan\Documents\LPCXpresso_8.2.2_650\workspace\Lib_CMSISv1p30_LPC17xx\inc/core_cm3.h:
+
+C:\Users\renan\Documents\LPCXpresso_8.2.2_650\workspace\Lib_CMSISv1p30_LPC17xx\inc/system_LPC17xx.h:
+
+C:\Users\renan\Documents\LPCXpresso_8.2.2_650\workspace\Lib_MCU\inc/lpc_types.h:
+
+C:\Users\renan\Documents\LPCXpresso_8.2.2_650\workspace\Lib_MCU\inc/lpc17xx_gpio.h:
+
+C:\Users\renan\Documents\LPCXpresso_8.2.2_650\workspace\Lib_MCU\inc/lpc17xx_i2c.h:
+
+C:\Users\renan\Documents\LPCXpresso_8.2.2_650\workspace\Lib_MCU\inc/lpc17xx_ssp.h:
+
+C:\Users\renan\Documents\LPCXpresso_8.2.2_650\workspace\Lib_MCU\inc/lpc17xx_adc.h:
+
+C:\Users\renan\Documents\LPCXpresso_8.2.2_650\workspace\Lib_MCU\inc/lpc17xx_timer.h:
+
+C:\Users\renan\Documents\LPCXpresso_8.2.2_650\workspace\Lib_EaBaseBoard\inc/joystick.h:
+
+C:\Users\renan\Documents\LPCXpresso_8.2.2_650\workspace\Lib_EaBaseBoard\inc/pca9532.h:
+
+C:\Users\renan\Documents\LPCXpresso_8.2.2_650\workspace\Lib_EaBaseBoard\inc/acc.h:
+
+C:\Users\renan\Documents\LPCXpresso_8.2.2_650\workspace\Lib_EaBaseBoard\inc/rotary.h:
+
+C:\Users\renan\Documents\LPCXpresso_8.2.2_650\workspace\Lib_EaBaseBoard\inc/led7seg.h:
+
+C:\Users\renan\Documents\LPCXpresso_8.2.2_650\workspace\Lib_EaBaseBoard\inc/oled.h:
+
+C:\Users\renan\Documents\LPCXpresso_8.2.2_650\workspace\Lib_EaBaseBoard\inc/rgb.h:
+
+C:\Users\renan\Documents\LPCXpresso_8.2.2_650\workspace\Lib_EaBaseBoard\inc/uart2.h:
+
+C:\Users\renan\Documents\LPCXpresso_8.2.2_650\workspace\Lib_MCU\inc/lpc17xx_uart.h:
